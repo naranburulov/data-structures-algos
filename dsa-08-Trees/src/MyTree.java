f4f27272b4fa10b62fea5f7e2759c8c6f4e3d366
naranburulov <naranburulov@gmail.com>
2022-11-19 14:19:54 -0500

    Trees

--- dsa-08-Trees/src/MyTree.java
+++ dsa-08-Trees/src/MyTree.java
@@ -1,8 +1,12 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MyTree {
     TNode root;
 
     public MyTree() {
     }
+
     void insert(int value){
         TNode newNode=new TNode(value);
         if (root==null) {
@@ -58,6 +62,100 @@ public class MyTree {
         postOrderTraversal(root.rightChild);
         System.out.print(root.value +", ");
     }
+
+
+    void levelOrderTraversal(){
+        if (root==null) return;
+        Queue<TNode>queue = new LinkedList<>();
+
+        queue.add(root);
+        while (!queue.isEmpty()){
+            TNode toVisit = queue.poll();
+            System.out.print(toVisit.value+", ");
+
+            if (toVisit.leftChild != null){
+                queue.add(toVisit.leftChild);
+            }
+            if (toVisit.rightChild != null){
+                queue.add(toVisit.rightChild);
+            }
+        }
+    }
+
+    public boolean contains(int value){
+        if (root==null) return false;
+        TNode current= root;
+        while (current!=null) {
+            if (value<current.value) current=current.leftChild;
+            else if (value> current.value) current=current.rightChild;
+            else return true;
+        }
+        return false;
+    }
+
+    public boolean isLeaf(TNode node) {
+        return node.leftChild==null && node.rightChild==null;
+    }
+
+    public void printLeaves(TNode root){
+        if (root==null) return;
+        // perform visit on Root
+
+        // Recursively Branch Left Subtree
+        printLeaves(root.leftChild);
+        printLeaves(root.rightChild);
+        if (isLeaf(root)) System.out.print(root.value + ", ");
+
+        // Recursively Branch Right Subtree
+    }
+    int countLeaves(TNode root){
+        if (root==null) return 0;
+        if (isLeaf(root)) return 1;
+        // recursively left
+        // recursively right
+        return countLeaves(root.leftChild) + countLeaves(root.rightChild);
+    }
+    int findSumOfLeaves(TNode root){
+        if (root==null) return 0;
+        if (isLeaf(root)) return root.value;
+        return findSumOfLeaves(root.leftChild) + findSumOfLeaves(root.rightChild);
+    }
+    int height(TNode root){
+        if (root==null) return -1;
+        if (isLeaf(root)) return 0;
+        return 1+ Math.max(height(root.leftChild), height(root.rightChild));
+    }
+
+    public int calculateNodeDepthSums(){
+        return nodeDepthSums(root,0);
+    }
+
+    public int nodeDepthSums(TNode node, int A){
+
+        if (node==null)return 0;
+        return A+nodeDepthSums(node.leftChild,A+1)+nodeDepthSums(node.rightChild,A+1);
+
+
+
+
+    }
+
+    public int calculateNodeSums(){
+        return nodeSums(root, root.value);
+    }
+
+    public int nodeSums(TNode node, int sum){
+
+        if (node==null)return 0;
+        return sum+nodeSums(node.leftChild,node.value)+nodeSums(node.rightChild,node.value);
+
+
+
+
+    }
+
+
+
 
 }
 
--- dsa-08-Trees/src/TreeApp.java
+++ dsa-08-Trees/src/TreeApp.java
@@ -1,26 +1,55 @@
+import org.w3c.dom.Node;
+
 public class TreeApp {
     public static void main(String[] args) {
         MyTree tree=new MyTree();
-        int[] numbers=new int[] {10, 5, 12, 3, 6, 11, 13, 2};
-        for (int i = 0; i <8; i++) {
+        int[] numbers=new int[] {10, 6, 8, 20, 4, 9, 5, 17, 42, 47, 29};
+        for (int i = 0; i <11; i++) {
             tree.insert(numbers[i]);
         }
-        VisualizeTree.printTree(tree.root,null,false);
+           VisualizeTree.printTree(tree.root,null,false);
+//
+//        System.out.println("Pre Order Traversal");
+//        tree.preOrderTraversal(tree.root);
+//
+//        System.out.println();
+//        System.out.println();
+//        System.out.println("In Order Traversal");
+//        tree.inOrderTraversal(tree.root);
+//
+//        System.out.println();
+//        System.out.println();
+//        System.out.println("Post Order Traversal");
+//        tree.postOrderTraversal(tree.root);
+//
+//        System.out.println();
+//        System.out.println();
+//        System.out.println("Level Order Traversal");
+//        tree.levelOrderTraversal();\
 
-        System.out.println("Pre Order Traversal");
-        tree.preOrderTraversal(tree.root);
+        System.out.println("contains - " + tree.contains(1));
 
-        System.out.println();
-        System.out.println();
-        System.out.println("In Order Traversal");
-        tree.inOrderTraversal(tree.root);
 
+
+
+
+        tree.printLeaves(tree.root);
         System.out.println();
-        System.out.println();
-        System.out.println("Post Order Traversal");
-        tree.postOrderTraversal(tree.root);
+        System.out.println("Number of leaves:  " +tree.countLeaves(tree.root));
+        System.out.println("Sum of Leaf Nodes: " + tree.findSumOfLeaves(tree.root));
+        System.out.println(tree.height(tree.root));
+
+
+        System.out.println("------------------");
+
+        System.out.println("sum of nodes depth: " + tree.calculateNodeDepthSums() );
+        System.out.println("sum of nodes' values: " + tree.calculateNodeSums());
 
 
 
     }
+
+
+
+
 }
