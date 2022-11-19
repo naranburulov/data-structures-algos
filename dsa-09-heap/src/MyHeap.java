44a24550daee194f9301848820941e68d32e3d2e
naranburulov <naranburulov@gmail.com>
2022-11-19 15:44:24 -0500

    My Heap - remove method completed

--- dsa-09-heap/src/MyHeap.java
+++ dsa-09-heap/src/MyHeap.java
@@ -20,6 +20,8 @@ public class MyHeap {
         return items[0];
     }
 
+    //insert:
+
     public void insert(int value){
         if (size ==items.length) throw new IllegalStateException();
         else {
@@ -54,8 +56,69 @@ public class MyHeap {
 
     public void printHeap(){
         for (int i = 0; i<size; i++){
-            System.out.println(items[i]+ ", ");
+            System.out.print(items[i]+ ", ");
         }
+        System.out.println();
+    }
+
+
+    //remove:
+
+    public int remove(){
+        if (size==0) throw new NoSuchElementException();
+        else {
+            int result = items[0];
+            items[0]=items[--size];
+            bubbleDown();
+            return result;
+        }
+    }
+
+    public void bubbleDown(){
+        int index=0;
+        int largerChildIndex;
+        while (index<=size && !isValidParent(index)){
+            largerChildIndex=largerChildIndex(index);
+            swap(index, largerChildIndex);
+            index=largerChildIndex;
+        }
+    }
+
+    public boolean isValidParent(int index){
+        if (!hasLeftChild(index)) return true;
+        else{
+            boolean isValid = false;
+            if (hasRightChild(index)){
+                isValid=(items[index]>=items[leftChildIndex(index)] &&
+                        items[index]>=items[rightChildIndex(index)]);
+            }
+            return isValid;
+        }
+    }
+
+    public boolean hasLeftChild(int index){
+        return leftChildIndex(index)<=size;
+    }
+    public boolean hasRightChild(int index){
+        return rightChildIndex(index)<=size;
+    }
+
+
+    public int leftChildIndex(int index){
+        return index*2+1;
+    }
+    public  int rightChildIndex(int index){
+        return index*2+2;
+    }
+
+
+    public int largerChildIndex(int index){
+        if (!hasLeftChild(index)) return index;
+        else if (!hasRightChild(index)) {
+            return leftChildIndex(index);
+        }
+        return items[leftChildIndex(index)]>items[rightChildIndex(index)] ? leftChildIndex(index): rightChildIndex(index);
+
     }
 
 }
