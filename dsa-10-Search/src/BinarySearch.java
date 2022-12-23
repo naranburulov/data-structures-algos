18ae9cb1846894b0be0a8d500ea73e127d7494fe
naranburulov <naranburulov@gmail.com>
2022-12-23 09:38:49 -0500

    added LinearSearch() to LinearSearch.class added binarySearchIterative() to BinarySearch.class

---
+++ dsa-10-Search/src/BinarySearch.java
@@ -0,0 +1,24 @@
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i]=i;
+        }
+        System.out.println(binarySearchIterative(numbers,6));
+    }
+
+    public static int binarySearchIterative(int[]array, int data){
+
+        int left=0;
+        int right= array.length-1;
+        while (left<=right){
+            int middle=(left+right)/2;
+            if (array[middle]==data) return middle;
+            if (data<array[middle]) right=middle-1;
+            else left=middle+1;
+        }
+        return -1;
+
+    }
+}
---
+++ dsa-10-Search/src/LinearSearch.java
@@ -0,0 +1,15 @@
+public class LinearSearch {
+    public int linearSearch(int[]array, int data){
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == data) return i;
+        }
+        return -1;
+    }
+
+
+
+
+
+
+}
