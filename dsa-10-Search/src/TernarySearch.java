6be8de7d123f3443abfc09be2cd7a89759b27078
naranburulov <naranburulov@gmail.com>
2022-12-23 10:17:01 -0500

    added ternarySearchRecursive() at TernarySearch.class

--- dsa-10-Search/src/TernarySearch.java
+++ dsa-10-Search/src/TernarySearch.java
@@ -19,4 +19,16 @@ public class TernarySearch {
             }
         }return -1;
     }
+
+    public static int ternarySearchRecursive(int[]array, int data, int left, int right) {
+        if (left > right) return -1;
+        int partition = (right - left) / 3;
+        int middle1 = left + partition;
+        int middle2 = right - partition;
+        if (array[middle1] == data) return middle1;
+        if (array[middle2] == data) return middle2;
+        if (data < array[middle1]) return ternarySearchRecursive(array, data, left, middle1 - 1);
+        if (data > array[middle2]) return ternarySearchRecursive(array, data, middle2 + 1, right);
+        else return ternarySearchRecursive(array, data, middle1 + 1, middle2 - 1);
+    }
 }
